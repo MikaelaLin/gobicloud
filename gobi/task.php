5336adc8dbffcab1a3c3cc3aecdde3e35c947d17
nurmerey <shakhanova.n@gmail.com>
2013-03-30 18:09:02 +1000

    entered isset statement

--- gobi/task.php
+++ gobi/task.php
@@ -1,7 +1,14 @@
 <?php
 
 //connect to the database
-
+if (!isset($_GET["taskname"]) || !isset($_GET["tasknote"]) || !isset($_GET["priority"]) || 
+        !isset($_GET["duedate"]) || !isset($_GET["duetime"]) || !isset($_GET["geolocation"]) || 
+        !isset($_GET["tag"]) || !isset($_GET["workspaceid"]) || !isset($_GET["userid"])
+        || !isset($_GET["timeflag"]) || !isset($_GET["status"]) || !isset($_GET["projectid"])
+        ) {
+    echo "Go away nothing to see here";
+    exit();
+}
 $mysqli = new mysqli("localhost", "root", "", "GOBI_DB");
 
 /* check connection */
@@ -16,27 +23,30 @@ if (mysqli_connect_errno()) {
     exit();
 }
 // set up values to insert
-$taskName = $Get["taskname"];
-$taskNote= $Get["tasknote"];
-$taskPriority= $Get["priority"];
-$dueDate = $Get["duedate"];
-$dueTime = $Get["duetime"];
-$geolocation = $Get["geolocation"];
-$tag = $Get["tag"];
-$workspaceID = $Get["workspaceid"];
-$userID = $Get["userid"];
-$timeFlag= $Get["timeflag"];
-$status = $Get["status"];
-$projectID = $Get["projectid"];
-$lastUpdate = $Get["lastUpdate"];
-$taskID = null;
+$taskName = $_GET["taskname"];
+
+$taskNote= $_GET["tasknote"];
+$taskPriority= $_GET["priority"];
+$dueDate = $_GET["duedate"];
+$dueTime = $_GET["duetime"];
+$geolocation = $_GET["geolocation"];
+$tag = $_GET["tag"];
+$workspaceID = $_GET["workspaceid"];
+$userID = $_GET["userid"];
+$timeFlag= $_GET["timeflag"];
+$status = $_GET["status"];
+$projectID = $_GET["projectid"];
+$lastUpdate = $_GET["lastUpdate"];
+
+echo 'show task name '.$_GET["taskname"].' ';
 
 //add data to Task table
 /* create a prepared statement */
 if ($stmt = $mysqli->prepare("INSERT INTO TASK VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")) {
 echo "prep";
     /* bind parameters for markers */
-    $stmt->bind_param("isiiisiississs", $taskID, $taskName, $workspaceID, $taskPriority, $userID, $dueDate, $timeFlag, $status, $geolocation, $tag, $projectID, $lastUpdate, $taskNote, $dueTime);   /* execute prepared statement */
+    $stmt->bind_param("isiiisiississs", '', $taskName, $workspaceID, $taskPriority, $userID, $dueDate, $timeFlag, $status, $geolocation, $tag, $projectID, '', $taskNote, $dueTime);   
+    /* execute prepared statement */
     $stmt->execute();
     
     //check for error
