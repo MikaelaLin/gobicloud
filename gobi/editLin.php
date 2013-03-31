1dcfba19d79ddc9c46fb9d43268da58d8f6733af
MikealaLin <mikeala.lin@gmail.com>
2013-03-31 16:34:33 +1000

    deleteTask.html

--- gobi/editLin.php
+++ gobi/editLin.php
@@ -1,10 +1,23 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+//connect to the database
+$mysqli = new mysqli("localhost", "root", "", "GOBI_DB");
 
-<!DOCTYPE html>
-<html><titile>HELLO world</titile></html>
+/* check connection */
+if (mysqli_connect_errno()) {
+    /* cannot connect to database, write fail to array */
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  
+    exit();
+}
+
+$query = "SELECT TASKID, TASKNAME, DUEDATE FROM TASK";
+$result = $mysqli->query($query);
+
+
+/* free result set */
+$result->free();
+//$_SESSION['taskID'] = $_GET($row["TASKID"])
+/* close connection */
+$mysqli->close();
+?>
